cf4c71a9661751f0fb0269f52c9b81f66f6a3cfc
jdinsh <jdinsh@gmail.com>
2021-04-25 20:55:54 -0400

    ReverseLinked list with bruteforce and recursive to save some time and memory
    ReverseLinked list with bruteforce and recursive to save some time

--- java-algorithms/algorithm/ReverseLinkedList.java
+++ java-algorithms/algorithm/ReverseLinkedList.java
@@ -1,6 +1,27 @@
 package algorithm;
 
 public class ReverseLinkedList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
 
-}
+        public ListNode reverseList(ListNode head) {
+            ListNode prev=null;
+            ListNode curr = head;
+            while(curr !=null){
+                ListNode nextTemp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextTemp;
+            }
+            return prev;
+        }
+
 }
